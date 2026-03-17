189878ef02028a9762aaaa369a17d2e325c7631a
Nadav011 <nadavcohen20838@gmail.com>
2026-03-17 15:26:45 +0200

    fix(ts): remove unused index variable in blog.tsx map

--- components/blog.tsx
+++ components/blog.tsx
@@ -225,7 +225,7 @@ export function Blog() {
 
         {/* Posts grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
-          {filtered.map((post, i) => (
+          {filtered.map((post) => (
             <a
               key={post.title}
               href={post.href}
